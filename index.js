814c82cc73a4a876813ef77d65e61600d0b4a4ae
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-08-14 09:39:53 +0530

    updating

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+var ready = false;
 
-window.captcha_ready = function () {
-    alert('captcha ready at captcha module');
+var pending = [];
+
+var captchaKey = $('#content').data('captcha-key');
+
+var render = function (el, options, done) {
+    var id = grecaptcha.render(el, {
+        sitekey: captchaKey,
+        callback: options.success,
+        'expired-callback': options.expired,
+        'error-callback': options.error
+    });
+    done(null, id);
+};
+
+window.serand_captcha_ready = function () {
+    ready = true;
+    pending.forEach(function (ctx) {
+        render(ctx.el[0], ctx.options, ctx.done);
+    });
+};
+
+module.exports.render = function (el, options, done) {
+    options = options || {};
+    if (!ready) {
+        return pending.push({el: el, options: options, done: done});
+    }
+    render(el[0], options, done);
+};
+
+module.exports.response = function (id, done) {
+    done(null, grecaptcha.getResponse(id));
 };
